f04d13969565d54ff964be529d10f86bf6c92672
David Bock <dbock@codesherpas.com>
2009-03-07 01:15:41 -0500

    extracting files from our existing deploy

---
+++ recipes/deploy_lock.rb
@@ -0,0 +1,34 @@
+before "deploy", "deploy:check_lock"
+before "deploy", "deploy:semaphor"
+after "deploy", "deploy:unlock"
+
+namespace :deploy do
+  def deploy_lockfile
+   "#{shared_path}/system/capistrano.lock"
+  end
+
+  task :semaphor do
+    set :lock_message, "Locked for depoyment by #{user}"
+    lock  
+  end
+  
+  task :lock do
+    caputils.ask :lock_message, "Lock Message: "
+    put lock_message, deploy_lockfile, :mode => 0777
+  end
+
+  task :unlock do
+    run "rm #{deploy_lockfile}"
+  end
+
+  task :check_lock do
+    lock_contents = ""
+    run "if [ -e #{deploy_lockfile} ]; then cat #{deploy_lockfile}; fi" do |ch, stream, out|
+      lock_contents = out
+    end
+    if !lock_contents.empty?
+      notify lock_contents unless lock_contents.empty?
+      raise lock_contents
+    end
+  end
+end
