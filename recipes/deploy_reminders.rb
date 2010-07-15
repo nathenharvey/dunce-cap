9f8f3edb10765b772b3df1f70455e85e42e67c73
bokmann <dbock@codesherpas.com>
2010-07-14 22:32:22 -0400

    namespacing things consistently

--- recipes/deploy_reminders.rb
+++ recipes/deploy_reminders.rb
@@ -1,27 +1,30 @@
-after "deploy", "reminders:show"
+after "deploy", "deploy:reminders:show"
 
-namespace :reminders do
-  def reminder_file
-   "#{shared_path}/system/reminders.txt"
-  end
+namespace :deploy do
+  namespace :reminders do
+    def reminder_file
+     "#{shared_path}/system/reminders.txt"
+    end
   
-  desc "adds a reminder that will be shown on deploy in the console of the next deployer"
-  task :add do  
-    set :reminder, Capistrano::CLI.ui.ask("Reminder: ")
-    run "echo \"user: #{user}\" >> #{reminder_file}"
-    run "echo \"On: #{Time.new.to_s}\" >> #{reminder_file}"
-    run "echo \"Message: #{reminder}\" >> #{reminder_file}"
-    run "echo \"\n\" >> #{reminder_file}"
-  end
+    desc "adds a reminder that will be shown on deploy in the console of the next deployer"
+    task :add do  
+      set :reminder, Capistrano::CLI.ui.ask("Reminder: ")
+      run "echo \"user: #{user}\" >> #{reminder_file}"
+      run "echo \"On: #{Time.new.to_s}\" >> #{reminder_file}"
+      run "echo \"Message: #{reminder}\" >> #{reminder_file}"
+      run "echo \"\n\" >> #{reminder_file}"
+    end
   
-  desc "shows all the current reminder messages"
-  task :show do
-    run "if [ -e #{reminder_file} ]; then cat #{reminder_file}; fi"
-  end
+    desc "shows all the current reminder messages"
+    task :show do
+      run "if [ -e #{reminder_file} ]; then cat #{reminder_file}; fi"
+    end
   
-  desc "clears the current reminder messages"
-  task :clear do
-    run "rm #{reminder_file}"
+    desc "clears the current reminder messages"
+    task :clear do
+      run "rm #{reminder_file}"
+    end
   end
-  
 end
+
+
